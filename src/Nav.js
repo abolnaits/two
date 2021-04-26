4f96279a7178e4622cbd8931f4e769ff98e1302f
Andres Benitez <abolnaits@hotmail.com>
2021-04-26 11:41:10 -0500

    App
    Fornite api

--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,28 @@
 import logo from './logo.svg';
 //import './App.css';
+import {Link} from 'react-router-dom'
 
 function Nav() {
-  return (
-    <nav>
-        <ul>
-            <li>About</li>
-            <li>Services</li>
-        </ul>
-    </nav>
-  );
+
+    const navStyle = {
+        color:'red'
+    }
+
+    return (
+        <nav>
+            <ul>
+                <Link to='/' style={navStyle}>
+                    <li>Home</li>
+                </Link>
+                <Link to='/about' style={navStyle}>
+                    <li>About</li>
+                </Link>
+                <Link to='/services' style={navStyle}>
+                    <li>Services</li>
+                </Link>
+            </ul>
+        </nav>
+    );
 }
 
 export default Nav;
--- src/Services.js
+++ src/Services.js
@@ -1,11 +1,36 @@
 import logo from './logo.svg';
 //import './App.css';
+import React, {useState,useEffect} from 'react';
 
 function Services() {
-  return (
+  
+    const fecthItems = () => {
+        fetch('https://fortnite-api.com/v1/playlists').then((response)=>{
+            console.log('Response =>',response);
+            return response.json();
+        }).then((data)=>{
+            console.log('Data =>',data);
+            setItems(data.data);
+        }).catch((e)=>{
+            console.log('Error =>',e);
+        });
+    }
+    
+    const [items,setItems] = useState([]);
+
+    useEffect(()=>{
+        fecthItems();
+    },[]);
+
+    return (
     <div>
       <header className="App-header">
         <h1>Services Page</h1>
+        {
+            items.map(item => (
+                <p key={item.id}>{item.id}</p>
+            ))
+        }
       </header>
     </div>
   );
